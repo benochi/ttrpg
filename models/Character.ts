17066e1079fcd5a498d0fa9f6a638d8e365ec138
Dan Polityka <73758508+benochi@users.noreply.github.com>
2025-06-10 22:49:16 -0500

    add unnequipped to char

--- models/Character.ts
+++ models/Character.ts
@@ -4,6 +4,7 @@ export interface ICharacter extends Document {
   name: string;
   level: number;
   inventoryEquipped: mongoose.Types.ObjectId;
+  inventoryUnequipped: mongoose.Types.ObjectId;
   createdAt: Date;
   updatedAt: Date;
 }
@@ -12,7 +13,8 @@ const CharacterSchema = new Schema<ICharacter>(
   {
     name: { type: String, required: true },
     level: { type: Number, default: 1 },
-    inventoryEquipped: { type: Schema.Types.ObjectId, ref: 'InventoryEquipped' } // update to inventory equipped an unequipped. 
+    inventoryEquipped: { type: Schema.Types.ObjectId, ref: 'InventoryEquipped' } // update to inventory equipped an unequipped.
+    inventoryUnequipped: {type: Schema.Types.ObjectId, ref: 'InventoryUnequipped'} 
   },
   { timestamps: true }
 );
